e060bd40b29fb432c9b0141fd43a0d537156a31f
DivljePragnje <tomicm990@gmail.com>
2021-03-08 20:25:36 +0100

    First commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { HomePage } from "./pages";
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <HomePage />
+      </main>
+    </div>
+  );
+}
+
+export default App;
